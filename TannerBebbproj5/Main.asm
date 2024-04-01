11493fe5999da53dbdc0b80fc6934f44ad5c9040
Tanner Bebb <17tbebb@gmail.com>
2024-04-01 08:54:28 -0700

    Added notes on line 24.

--- TannerBebbproj5/Main.asm
+++ TannerBebbproj5/Main.asm
@@ -15,10 +15,17 @@ str_len = 10																;This is just set to the value that I want each leng
 .code
 main proc
 
-	call Clrlf                                                              ;This is going to clear the line.
+	call Clrscr                                                             ;This is going to clear the line.
     mov esi, OFFSET str_index                                               ;This is moving the address location of str_index to the esi register.
     call Randomize                                                          ;Unsure as to what this does right now. I am looking into it.
     mov ecx, 20                                                             ;This will move the value of 20 to the ecx register. This is the number of strings I want this to generate.
+
+L1:
+    call Random32                                                           ;Ran out of time for this session. I will finish the documentation later
+    call CreateRandomString                                                 ;
+    loop L1                                                                 ;
+
+    invoke ExitProcess,0
 
 main endp
 
