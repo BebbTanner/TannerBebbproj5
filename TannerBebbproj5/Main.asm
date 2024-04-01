d0faba2e54f1a99a6943c5b01ac43f853fb488b4
Tanner Bebb <17tbebb@gmail.com>
2024-04-01 08:41:24 -0700

    Declared and defined the variable str_len. In the data section I added the command that is going to duplicate the string that I am planning on generating.

--- TannerBebbproj5/Main.asm
+++ TannerBebbproj5/Main.asm
@@ -7,8 +7,10 @@
 include irvine32.inc
 ExitProcess proto, dwExitCode:dword
 
-.data
+str_len = 10																;This is just set to the value that I want each length of the string to be.
 
+.data
+	str_index BYTE str_len DUP(0),  0										;This is going to duplicate the string a given number of times.
 
 .code
 main proc
