d7a5c81ff6499ec2212d915b420d901e057d047b
Tanner Bebb <17tbebb@gmail.com>
2024-04-17 08:49:33 -0700

    I did not commit the whole process, but I am trying a different set up for this program. Once again I am really close to figuring it out.

--- TannerBebbproj5/Main.asm
+++ TannerBebbproj5/Main.asm
@@ -1,49 +1,52 @@
-;Setting up a test to print out a string 20 times instead of counting from 1 to 20.
-;Currently, it is printing out the string 20 times, but it is printing it backwords and idk why.
-;In the notes it also says to use CALL READCHAR and CALL WRITESTRING
-
 INCLUDE Irvine32.inc
 
+STR_COUNT = 20
+STR_SIZE = 10
 str_len = 9
 
 .data
-;	move dx, OFFSET prompt						;This was in the notes for this assignment
-	str_index BYTE str_len DUP(0), 0
-	i SDWORD ?
+    aString BYTE STR_SIZE DUP(0),0
+    str_index BYTE str_len DUP(0),  0
+    i SDWORD ?
 
 .code
-
 main PROC
-	mov i, 1
-again:
-	cmp i, 20
-	jg done
-	mov eax, ebx
-	call WriteInt
-	mov al, ' '
-	call WriteChar
-	inc i
-	jmp again
+    mov ecx,STR_COUNT                                   ; loop count
+
+L1:                                                     ; generates a single string
+    mov eax,STR_SIZE                                    ; loop counter
+    mov esi,OFFSET aString                              ; string index
+
+    call CreateRandomString
+    mov edx,OFFSET aString                              ; display the string
+    call Crlf
+    cmp eax, 20
+    jb L1
+    jg done
+;    loop L1
+
 done:
-	
-	call Crlf
-	call WaitMsg
 
-;This is going to generate the random string that is a length of 10 characters.
-mov ecx, LENGTHOF str_index                     ;This is moving the number of elements of str_index to the ecx register.
-L2:
-    mov eax, 26                                 ;Moving the value 26 to the eax register. It is 26 because the is the total number of letters in the alphabet.
-    call RandomRange                            ;This will generate a random unassigned random integer.
-    add eax, 65                                 ;This is going to add 65 to number generated in the eax register. The reason it does this is to make all of the letters their capital counterparts.
-    mov ebx, eax                                ;This is moving whatever is in the eax register to the esi register. The esi register has been dereferanced.
-    call WriteChar                              ;Write the current charaters to the screen.
-    loop L2                                     ;loop back to L2, and repeat the process until the whole string has been generated.
-    call Crlf                                   ;Add a new line.
-    ret                                         ;Return to the main proc after the whole process is finished.
-
-	exit
+    exit
 
 main ENDP
 
+;-------------------------------------------
 
-END	main
+CreateRandomString PROC
+
+mov ecx, LENGTHOF str_index
+    L2:
+        mov eax, 26
+        call RandomRange
+        add eax, 65
+        mov[esi], eax
+        call WriteChar
+        loop L2
+    call Crlf
+    ret
+
+CreateRandomString ENDP
+
+
+END main
