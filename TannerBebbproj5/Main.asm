9c3369ee071ce6e408d534d00fac576a7a9352e4
Tanner Bebb <17tbebb@gmail.com>
2024-04-17 09:17:05 -0700

    Finally got the result I was looking for. Cleaned up the program.
    Started setup for the documentation.

--- TannerBebbproj5/Main.asm
+++ TannerBebbproj5/Main.asm
@@ -1,51 +1,48 @@
 ;Tanner Bebb Project 5 assembly language
-;So far this prints out 1 string of 10 random characters. I need the loop to work correctly.
-;At first I was pretty sure that it was an issue in the L1 tag. But now I am almost wondering if it is an issue under the L2 tag.
 
 INCLUDE Irvine32.inc
 
-STR_COUNT = 20
-STR_SIZE = 10
+str_size = 10
 str_len = 9
 
 .data
-    aString BYTE STR_SIZE DUP(0),0
-    str_index BYTE str_len DUP(0),  0
-    i SDWORD ?
+    arr1 BYTE str_size DUP(0),0                         ;
+    str_index BYTE str_len DUP(0),  0                   ;
+    str_count SDWORD ?                                  ;
 
 .code
 main PROC
-    mov ecx,STR_COUNT                                   ; loop count
+    mov str_count, 1                                    ;
 
-L1:                                                     ; generates a single string
-    mov eax,STR_SIZE                                    ; loop counter
-    mov esi,OFFSET aString                              ; string index
-    call CreateRandomString                             ; Calls the random string function
-    mov edx,OFFSET aString                              ; display the string
-;   cmp eax, 20
-;   jb L1
-;   jg done
+L1:                                                     ;
+    cmp str_count, 20                                   ;
+    jg done                                             ;
+    mov eax,str_size                                    ;
+    mov esi,OFFSET arr1                                 ;
+    call CreateRandomString                             ;
+    mov edx,OFFSET arr1                                 ;
+    inc str_count                                       ;
+    jmp L1                                              ;
 
 done:
-	Call Crlf
-	Call WaitMsg
-	exit
+	Call Crlf                                           ;
+	Call WaitMsg                                        ;
+	exit                                                ;
 
-main ENDP
+main ENDP                                               ;
 
 CreateRandomString PROC
-
-    mov ecx, LENGTHOF str_index
+    mov ecx, LENGTHOF str_index                         ;
 L2:
-    mov eax, 26
-    call RandomRange
-    add eax, 65
-    mov[esi], eax
-    call WriteChar
-    loop L2
-    call Crlf
-    ret
+    mov eax, 26                                         ;
+    call RandomRange                                    ;
+    add eax, 65                                         ;
+    mov[esi], eax                                       ;
+    call WriteChar                                      ;
+    loop L2                                             ;
+    call Crlf                                           ;
+    ret                                                 ;
 
-CreateRandomString ENDP
+CreateRandomString ENDP                                 ;
 
 END main
